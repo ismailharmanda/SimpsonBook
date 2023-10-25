ec474fd8c1140d9bafbaa7d2cbdc3a89eb2c2117
İsmail Harmanda <iharmanda@hotmail.com>
2023-10-26 01:23:27 +0300

    create navigation and add outlets

---
+++ SimpsonBook/DetailVC.swift
@@ -0,0 +1,36 @@
+//
+//  DetailVC.swift
+//  SimpsonBook
+//
+//  Created by ismail harmanda on 26.10.2023.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    
+    
+    
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
