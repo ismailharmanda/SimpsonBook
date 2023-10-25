b430db5ceb6b4fe1eec49cadc3c60214eb9dc7eb
İsmail Harmanda <iharmanda@hotmail.com>
2023-10-26 02:05:57 +0300

    render simpsons in listview

--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -9,14 +9,23 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    let Homer = Simpson(name: "Homer Simpson", job: "Nuclear Safety", image: "homer")
+    let Bart = Simpson(name: "Bart Simpson", job: "Student", image: "bart")
+    let Maggie = Simpson(name: "Maggie Simpson", job: "Baby", image: "maggie")
+    let Marge = Simpson(name: "Marge Simpson", job: "Housewife", image: "marge")
+    let Lisa = Simpson(name: "Lisa Simpson", job: "Student", image: "lisa")
+    
+    var simpsons: [Simpson] = [Simpson]()
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        simpsons.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = "Test"
+        content.text = simpsons[indexPath.row].name
+        content.secondaryText = simpsons[indexPath.row].job
         
         cell.contentConfiguration = content
         return cell
@@ -32,6 +41,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        simpsons = [Homer, Bart, Maggie, Marge, Lisa]
     }
     
     
