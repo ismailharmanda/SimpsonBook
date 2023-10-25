11ad7c8aa8c63b1c459de6fbb0c3aaf8fd3c2208
İsmail Harmanda <iharmanda@hotmail.com>
2023-10-26 01:29:19 +0300

    create tableview test cells

--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Test"
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -15,6 +29,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     
